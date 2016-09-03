4d7a8ef7eeeb9ac44b5097635e20d883ed58e0aa
Chainsaw Bucktooth <email": null>
2016-09-03 12:58:09 -0400

    build NavBar component and add Bootstrap stlying that toggles between links, such as Log Out, About, Home. Use ternary operator to show different views depending on whether a user is logged in or not

--- app/assets/javascripts/components/NavBar.es6.jsx
+++ app/assets/javascripts/components/NavBar.es6.jsx
@@ -3,26 +3,42 @@ class NavBar extends React.Component{
 
   render(){
     return(
-      <nav className="navbar">
-        { this.props.currentUser ?
-        <ul>
-          <li>
-              <form  method="post" action="/users/sign_out">
-                <input id="logoutButton" type="Submit" value="Log Out"/>
-                <input type="hidden" name="_method" value="Delete"/>
-              </form>
-          </li>
-          <li><a href='/about'>About</a></li>
-          <li><a href='/'>Home</a></li>
-        </ul>
-        :
-        <ul>
-          <li><a href='/users/sign_in'>Sign In</a></li>
-          <li><a href='/users/sign_up'>Register</a></li>
-          <li><a href='/about'>About</a></li>
-          <li><a href='/'>Home</a></li>
-        </ul>
-      }
+      <nav className="navbar navbar-default navbar-fixed-top">
+         <div className="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                 <span class="sr-only">Toggle navigation</span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                </button>
+              <a class="navbar-brand" href="/">Framework!</a>
+            </div>
+          { this.props.currentUser ?
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">About<span className="sr-only">(current)</span></a></li>
+              <li>
+                  <form  method="post" action="/users/sign_out">
+                    <input id="logoutButton" type="Submit" value="Log Out"/>
+                    <input type="hidden" name="_method" value="Delete"/>
+                  </form>
+              </li>
+              <li><a href='/'>Home</a></li>
+            </ul>
+          </div>
+          :
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
+              <li><a href='/users/sign_in'>Sign In</a></li>
+              <li><a href='/users/sign_up'>Register</a></li>
+              <li><a href='/about'>About</a></li>
+              <li><a href='/'>Home</a></li>
+            </ul>
+          </div>
+        }
+        </div>
       </nav>
     )
   }
