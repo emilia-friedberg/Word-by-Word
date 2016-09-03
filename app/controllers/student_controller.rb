798bf2b84d6f2060022c8538f53bddea05e37372
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-09-03 12:39:55 -0400

    write show method for students

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,6 +1,13 @@
 class StudentsController < ApplicationController
   def show
     student = Student.find(params[:id])
-
+    if student.cohorts.nil?
+      student_belongs_to_cohort = false
+    else
+      student_belongs_to_cohort = true
+    end
+    cohorts = student.cohorts
+    student_hash = {student: student, studentBelongsToCohort: student_belongs_to_cohort, studentCohorts: cohort}
+    render json: student_hash.to_json
   end
 end
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,6 +2,11 @@ class Assignment < ApplicationRecord
   belongs_to :cohort
   belongs_to :lesson
 
+  def overdue?
+    return true if this.due_date.past?
+    return false
+  end
+
   validates :cohort_id, presence: true
   validates :lesson_id, presence: true
 
