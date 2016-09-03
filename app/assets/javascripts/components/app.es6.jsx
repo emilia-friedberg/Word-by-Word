c3a14981096ae975940f3afed34b90e7cadf2bd0
Gabriel Mahan <mahan.gabe@gmail.com>
2016-09-03 12:24:25 -0400

    update with init react

---
+++ app/assets/javascripts/components/app.es6.jsx
@@ -0,0 +1,10 @@
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        this is home app component
+      </div>
+    )
+  }
+}
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    render html: "hello world!"
   end
 
 
