a5a988ff4cd3847254e486f8a0f5981795c58d46
Chainsaw Bucktooth <email": null>
2016-09-03 16:01:53 -0400

    revert back to master

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'application#index'
-  # get 'practice/show'
 
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions'}
+  get 'practice/show'
+
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-     get '/users/sign_in' => 'devise/sessions#create'
-     get '/users/sign_up' => 'users/registrations#new'
   get 'students/:id', to: 'students#show'
   get 'teachers/:id', to: 'teachers#show'
   get '/practice/units/:unit_id/lessons/:lesson_id', to: 'practice#show'
-  resources :cohorts, :assignments
-
+  resources :cohorts
+  resources :assignments
+  root to: 'application#index'
 end
