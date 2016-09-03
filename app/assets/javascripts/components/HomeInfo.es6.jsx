de9b3cd6f7696c3fbfd9aff8c35bcf945bf97a2e
Chainsaw Bucktooth <email": null>
2016-09-03 17:06:33 -0400

    Initial commit - create component that has a standard introduction to web app

---
+++ app/assets/javascripts/components/HomeInfo.es6.jsx
@@ -0,0 +1,12 @@
+class HomeInfo extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <p>
+          Founded by three former teachers in 2016, Framework&#169; offers English grammar instruction to grade school students. The curriculum is broadly organized into units, which are then divided into lessons. The drag-and-drop interface allows for easy interaction with course material.  The teaching module breaks down a sentences into their essential components and assesses which steps in the process the student got wrong, helping teachers identify gaps in learning and develop individualized curriculum. Teachers and students can use Framework to supplement classroom instruction.  Teachers can organize their students into virtual classrooms, assign their students homework and monitor individual progress. 
+        </p>
+      </div>
+    )
+  }
+}
