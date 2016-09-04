f1ae8978a5a93d418d5b2b243cd1b049f3f67a68
Chainsaw Bucktooth <email": null>
2016-09-04 11:32:10 -0400

    adjust NavBar styling, adjusted Bootstrap defaults to display the NavBar properly, add test logo to NavBar, split NavBar into left adn right sections

--- app/assets/javascripts/components/NavBar.es6.jsx
+++ app/assets/javascripts/components/NavBar.es6.jsx
@@ -19,8 +19,10 @@ class NavBar extends React.Component{
           </div>
           :
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-left">
+            <a class="navbar-brand" href="/"><img id="brand-logo" src="/assets/test_logo_2.png"/></a>
+          </ul>
            <ul className="nav navbar-nav navbar-right">
-              <li className="active"><a href='/'><span className="sr-only">Home</span></a></li>
               <li><a href='/about'>About</a></li>
               <li><a href='/users/sign_up'>Register</a></li>
               <li><a href='/users/sign_in'>Sign In</a></li>
