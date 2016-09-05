d372bedc204fcd4ffdaec91b6b1a0533c4328dbd
Chainsaw Bucktooth <email": null>
2016-09-05 14:20:27 -0400

    Add Ruby partial to home page to enable users to log in directly from the home page

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -3,10 +3,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <NavBar currentUser={this.props.currentUser}/>
-        <HomeInfo/>
         <p>this is the home app component</p>
-        <Footer/>
       </div>
     )
   }
