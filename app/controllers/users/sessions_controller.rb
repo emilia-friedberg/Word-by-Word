e2fbbbcd256cd44af899911a9a1313dfae4330ea
Chainsaw Bucktooth <email": null>
2016-09-05 19:18:25 -0400

    add private function to both the Application and Session controllers to redirect users to /practice/UnitOne/Lesson1 upon sign in, for both newly-registered users and existing users

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -23,4 +23,5 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
 end
