d7136b1631750ce0409642c96ab1516d684a8a69
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-09-05 19:26:06 -0400

    some stuff

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,14 +1,26 @@
 class UnitsController < ApplicationController
   def unit_list
     units = Unit.all.map do |unit|
-      {
-        name: unit.name,
-        lessons: unit.lessons
+      lessons = unit.lessons.map do |lesson|
+          {
+            name: lesson.name,
+            id: lesson.id
+          }
+        end
+    return  {
+      name: unit.name,
+      lessons: lessons
       }
     end
+
     units_hash = {
       units: units
     }
     render json: units_hash.to_json
+  end
+
+  def lesson_show
+    # this needs to be changed to accomodate the params....
+    render :'unitone/three'
   end
 end
