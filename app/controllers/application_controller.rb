511ec10e90734b606f4063fac9e7e5a35ed4ebf0
Chainsaw Bucktooth <email": null>
2016-09-06 11:34:18 -0400

    modify application and registration controllers by creating private method after_sign_in_path_for that sends user to his or her show page after logging in or registering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
 private
   def after_sign_in_path_for(resource)
-    if current_user.status
+    if current_user.status == "Teacher"
       "/teachers/#{current_user.id}"
     else
       "/students/#{current_user.id}/info"
