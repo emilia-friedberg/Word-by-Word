2d8aec11e30c711b63a02ecaea24261262c93adf
Chainsaw Bucktooth <email": null>
2016-09-07 19:05:22 -0400

    recompiled assets

---
+++ public/assets/components/status_bar-384f41a8836a5ac3b76d0d5f7714a365dcc71599211643dde7d6ac6bdde674ac.jsx
@@ -0,0 +1,13 @@
+class StatusBar extends React.Component {
+  render() {
+      return (
+        <div id="statusBar">
+          <ul>
+            <li> 	&#9734; Hot Streak: {this.props.streak}  |</li>
+            <li> Total Correct: {this.props.totalCorrect}  |</li>
+            <li> Total Attempts: {this.props.totalAttempts} </li>
+          </ul>
+        </div>
+      )
+  }
+}
