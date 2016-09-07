ee912f1164084e516a29c28536d785b06eeab387
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-09-07 13:51:12 -0400

    refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,64 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-300.times do
 
-  new_s = Student.new(email: Faker::Internet.email,
-              password: Faker::Internet.password,
-              first_name: Faker::Name.first_name,
-              last_name: Faker::Name.last_name,
-              display_name: Faker::Superhero.name
+25.times do
+  Student.create(
+                  email: Faker::Internet.email,
+                  password: Faker::Internet.password,
+                  first_name: Faker::Name.first_name,
+                  last_name: Faker::Name.last_name,
+                  display_name: Faker::Superhero.name
                 )
-  new_s.save
 end
 
 puts "students created"
 
-25.times do
-  Teacher.create(email: Faker::Internet.email,
-              password: Faker::Internet.password,
-              first_name: Faker::Name.first_name,
-              last_name: Faker::Name.last_name,
-              display_name: Faker::Superhero.name)
-
-end
-
+Teacher.create(
+                email: 'mahan.gabe@gmail.com',
+                password: 'bella234',
+                first_name: 'Gabriel',
+                last_name: 'Mahan',
+                display_name: 'Mr. Mahan'
+              )
+Teacher.create(
+                email: 'emiliafriedberg@gmail.com',
+                password: 'IloveMyra',
+                first_name: 'Emilia',
+                last_name: 'Friedberg',
+                display_name: 'Ms. Friedberg'
+              )
 puts "teachers created"
 
-15.times do
-  new_cohort = Cohort.create(name: Faker::Team.name, access_code: Faker::Code.asin)
-  Teacher.all.sample.cohorts << new_cohort
-  Student.all.sample(25).each { |s| s.cohorts << new_cohort }
-end
-
+new_cohort = Cohort.create(name: "Ms. Friedberg and Mr. Mahan's class", access_code: 'nextGen')
+Teacher.all.each {|teacher| teacher.cohorts << new_cohort }
+Student.all.each { |student| student.cohorts << new_cohort }
 puts "cohorts created"
 
-15.times do
-  Child.create(teacher_id: Teacher.all.sample.id, cohort_id: Cohort.all.sample.id, access_code: Faker::Code.asin)
-end
 
+5.times do
+  Child.create(teacher_id: Teacher.all.sample.id, cohort_id: Cohort.all.sample.id, access_code: 'nextGen')
+end
 puts "child (orphans) created"
 
-Unit.create(name: 'test development unit', description: "this unit is for testing purposes")
-5.times do
-  Lesson.create(name: Faker::Hacker.ingverb + Faker::Hacker.noun, unit_id: Unit.first.id, common_core_standard: "CC 2.4")
-end
+Unit.create(name: 'Fundamentals: Subjects, Verbs, and Objects', description: "This unit covers basic identification of subjects, verbs, and objects")
+Lesson.create(name: "Subject Identification", unit_id: Unit.first.id, common_core_standard: "CCSS.ELA-LITERACY.L.3.1.A")
+Lesson.create(name: "Verb Identification", unit_id: Unit.first.id, common_core_standard: "CCSS.ELA-LITERACY.L.3.1.A")
+Lesson.create(name: "Subject and Verb Identification", unit_id: Unit.first.id, common_core_standard: "CCSS.ELA-LITERACY.L.3.1.A")
+Lesson.create(name: "Object Identification", unit_id: Unit.first.id, common_core_standard: "CCSS.ELA-LITERACY.L.3.1.A")
+Lesson.create(name: "Subject, Verb, and Object Identification", unit_id: Unit.first.id, common_core_standard: "CCSS.ELA-LITERACY.L.3.1.A")
 
 puts "units created"
 
-50.times do
+10.times do
   Assignment.create!(cohort_id: Cohort.all.sample.id, lesson_id: Lesson.all.sample.id, due_date: Faker::Date.between(2.days.from_now, 10.days.from_now))
 end
 
