dbd21b7f5abf3ff989789dab74a220f9299ef4d7
Gabriel Mahan <mahan.gabe@gmail.com>
2016-09-07 21:06:21 -0400

    made a glossary show

--- app/assets/javascripts/components/glossary.es6.jsx
+++ app/assets/javascripts/components/glossary.es6.jsx
@@ -1,11 +1,15 @@
 class Glossary extends React.Component {
   constructor() {
-    super()
-    this.state = { showGlossary: false }
+    super();
+    this.state = {
+      showGlossary: false
+     }
     this.toggleGlossary = this.toggleGlossary.bind(this)
+    this.showPage = this.showPage.bind(this)
+
   }
 
-  toggleGlossary() {
+  toggleGlossary(ev, n) {
     if (this.state.showGlossary) {
       this.setState({showGlossary: false})
     }
@@ -13,6 +17,13 @@ class Glossary extends React.Component {
       this.setState({showGlossary: true})
     }
   }
+
+  showPage(ev) {
+    ev.preventDefault()
+    debugger;
+    this.refs[ev.target.id].className = "activeGlossary"
+  }
+
 
 
   render() {
@@ -22,6 +33,20 @@ class Glossary extends React.Component {
         <div id="glossaryFill">
           <div id="backButton" className="glossaryButton" onClick={this.toggleGlossary}>
             Back!
+          </div>
+          <div id="helpPageContainer">
+            <div ref="glossaryList" id="glossaryList" >
+              <ul>
+                <li id="1" onClick={this.showPage}> Sentences          </li>
+                <li id="2" onClick={this.showPage}> Subjects and verbs </li>
+                <li id="3" onClick={this.showPage}> Objects            </li>
+              </ul>
+            </div>
+            <div id="glossaryShow">
+              <GlossarySentences ref="1"/>
+              <GlossarySubjects ref="2"/>
+              <GlossaryObjects ref="3"/>
+            </div>
           </div>
         </div>
       :
