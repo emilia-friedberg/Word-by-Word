b5fe7cf51e26d2d3a71669730f414149f9a33120
Gabriel Mahan <mahan.gabe@gmail.com>
2016-09-08 13:35:57 -0400

    initial styling

--- app/assets/javascripts/components/NavBar.es6.jsx
+++ app/assets/javascripts/components/NavBar.es6.jsx
@@ -2,7 +2,7 @@ class NavBar extends React.Component{
 
   render(){
     return(
-      <nav className="navbar navbar-default">
+      <nav id="nb" className="navbar navbar-default">
           <div className="navbar-header">
             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
               <span className="sr-only">Toggle navigation</span>
