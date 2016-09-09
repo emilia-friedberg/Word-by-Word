f310dcefd69ba6784c8728e02a29c793b872ae91
Gabriel Mahan <mahan.gabe@gmail.com>
2016-09-09 14:58:16 -0400

    continue minor UI tweaks

--- app/assets/javascripts/components/status_bar.es6.jsx
+++ app/assets/javascripts/components/status_bar.es6.jsx
@@ -3,9 +3,9 @@ class StatusBar extends React.Component {
       return (
         <div id="status-bar">
           <ul>
-            <li> 	&#9734; Hot Streak: {this.props.streak}  |</li>
-            <li> Total Correct: {this.props.totalCorrect}  |</li>
-            <li> Total Attempts: {this.props.totalAttempts} </li>
+            <li> 	&#9734; Hot Streak: {this.props.streak} |</li>
+            <li>  Total Correct: {this.props.totalCorrect} |</li>
+            <li> Total Attempts: {this.props.totalAttempts}</li>
           </ul>
         </div>
       )
