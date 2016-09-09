68ab55eb34669a4e9a643d1e9b4ad2aab6963e4a
Gabriel Mahan <mahan.gabe@gmail.com>
2016-09-09 14:36:45 -0400

    little

--- app/assets/javascripts/components/glossary/glossary_sentences.es6.jsx
+++ app/assets/javascripts/components/glossary/glossary_sentences.es6.jsx
@@ -25,13 +25,7 @@ class GlossarySentences extends React.Component {
         or if the verb has a different subject, as in John - I can't stand him!, then 'John' is not considered
         to be the grammatical subject, but can be described as the 'topic' of the sentence.
 
-        These definitions seem clear enough for simple sentences such as the above, but as will be shown in
-        the article below, problems in defining the subject arise when an attempt is made to extend the definitions
-        to more complex sentences and to languages other than English. For example, in the sentence It is difficult
-        to learn French, the grammatical subject seems to be the word 'it', and yet arguably the 'real' subject
-        (the thing that is difficult) is 'to learn French'. (A sentence such as It was John who broke the window
-        is more complex still.) Sentences beginning with a locative phrase, such as There is a problem, isn't
-        there?, in which the tag question 'isn't there?' seems to imply that the subject is the adverb 'there'
+      
       </div>
 : <div> </div> }
       </div>
