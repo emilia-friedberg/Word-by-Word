12a4e3d0e13aacd7960eb67d8b93dc43b6d983cf
Emilia Friedberg <emiliafriedberg@gmail.com>
2016-09-28 14:16:35 -0400

    nav bar fix in progress

--- app/assets/javascripts/components/NavBar.es6.jsx
+++ app/assets/javascripts/components/NavBar.es6.jsx
@@ -13,10 +13,10 @@ class NavBar extends React.Component{
           </div>
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav navbar-right">
-              <li id="navbar-buttons"><a id="navbar-links" href="/dashboard">My Dashboard</a></li>
-              <li id="navbar-buttons">
-                  <form  method="post" action="/users/sign_out">
-                    <input id="navbar-links" type="Submit" value="Log Out"/>
+              <li className="navbar-buttons"><a className="navbar-links" href="/dashboard">My Dashboard</a></li>
+              <li className="navbar-buttons">
+                  <form method="post" action="/users/sign_out">
+                    <input className="navbar-links" type="Submit" value="Log Out"/>
                     <input type="hidden" name="_method" value="Delete"/>
                   </form>
                 </li>
